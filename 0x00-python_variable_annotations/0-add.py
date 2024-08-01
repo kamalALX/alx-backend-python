9f5e364e2a6556ca94227acfc1b832e3c4c7b29a
Kamal <kamalmiftah01@gmail.com>
2024-08-01 12:10:20 +0100

    task solution

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python3
+"""
+    a add function that takes 2 arguments of type float and
+    returns a float value.
+"""
+
+
 def add(a: float, b: float) -> float:
     return float(a + b)
