a0cfc25fb0965aadcd62540de481cefe1b8a45b4
Kamal <kamalmiftah01@gmail.com>
2024-08-01 14:56:07 +0100

    task solution

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,12 +2,12 @@
 """ type-annotated function sum_list"""
 
 
-def sum_list(input_list : List[float]) -> float:
+def sum_list(input_list : list[float]) -> float:
     """
         sum_list takes a list input_list of floats as argument and
         returns their sum as a float.
     """
-    sum : float = 0.0
+    sum: float = 0.0
     for value in input_list:
         sum = value + sum
 
